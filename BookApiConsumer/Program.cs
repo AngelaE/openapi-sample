fb72bae7ae1eaa06b609ff63f76952f0dbae0dce
AngelaE <angela.koepp@gmail.com>
2020-12-06 21:54:56 +1300

    create book api client and sample consumer

---
+++ BookApiConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Clients;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookApiConsumer
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var client = new BookApiClient(httpClient, false)
+            {
+                BaseUri = new Uri("http://localhost:5000")
+            };
+
+            var authors = await client.Authors.GetAllAuthorsAsync();
+
+            foreach (var author in authors)
+            {
+                Console.WriteLine($"Name: {author.Name}");
+            }
+
+            return 0;
+        }
+    }
+}
