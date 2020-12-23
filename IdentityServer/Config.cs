7a68e825c571b2d517c6eaaf80da9a584b0e6d25
Angela <angela.koepp@gmail.com>
2020-12-23 21:41:00 +1300

    Add Identity Server to solution

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { new ApiScope("book-api.full-access")};
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            { new Client
+        {
+            ClientId = "book-api-consumer",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "book-api.full-access" }
+        }};
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                    new ApiResource("book-api", "My Book API")
+                    {
+                        Scopes = { "book-api.full-access"}
+                    }
+            };
+    }
+}
