4ad2863260817c335bd5c89dcf6fe7818b48eab0
Angela <angela.koepp@gmail.com>
2020-12-23 23:47:22 +1300

    Add credentials to the typescript consumer

--- BookApiConsumer.TypeScript/src/call-api.ts
+++ BookApiConsumer.TypeScript/src/call-api.ts
@@ -1,9 +1,23 @@
 import { BookApiClient } from './book-api-client/bookApiClient'
+import * as superAgent from 'superagent';
+import { TokenCredentials } from '@azure/ms-rest-js';
+
 
 async function QueryAuthors() {
-    const client = new BookApiClient({ baseUri: 'http://localhost:5000' });
-    const authors = await client.authors.getAll();
-    authors.forEach(author => console.log(`Author: ${author.name}`))
+    let accessToken;
+
+    try {
+        const response = await superAgent.post('http://localhost:5000/connect/token')
+            .send('client_id=book-api-consumer&client_secret=secret&grant_type=client_credentials');
+
+        const client = new BookApiClient(new TokenCredentials(response.body.access_token),
+            { baseUri: 'http://localhost:5001' });
+        const authors = await client.authors.getAll();
+        authors.forEach(author => console.log(`Author: ${author.name}`))
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 QueryAuthors();
