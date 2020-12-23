3849aa6eb3bb6e947c34bee8953143f85ade1f0e
Angela <angela.koepp@gmail.com>
2020-12-23 22:25:14 +1300

    Add credentials to c# consumer

--- BookApiConsumer/Program.cs
+++ BookApiConsumer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Clients;
+using IdentityModel.Client;
+using Microsoft.Rest;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,12 +12,41 @@ namespace BookApiConsumer
         static async Task<int> Main(string[] args)
         {
             var httpClient = new HttpClient();
-            var client = new BookApiClient(httpClient, false)
+            var disco = await httpClient.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return 1;
+            }
+
+            // request token
+            // client credentials would be used in service-to-service authentication and the
+            // returned token would most likely be a relatively long lived JWT
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "book-api-consumer",
+                ClientSecret = "secret",
+                Scope = "book-api.full-access"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return 1;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var apiClient = new BookApiClient(new TokenCredentials(tokenResponse.AccessToken), 
+                httpClient, false)
             {
                 BaseUri = new Uri("http://localhost:5001")
             };
 
-            var authors = await client.Authors.GetAllAsync();
+            // query API
+            var authors = await apiClient.Authors.GetAllAsync();
 
             foreach (var author in authors)
             {
