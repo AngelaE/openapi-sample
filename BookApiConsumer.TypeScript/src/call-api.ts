5968cfa174ea7b6a04511e7d31e592efa5c0f201
AngelaE <AngelaE@users.noreply.github.com>
2021-01-09 12:31:04 +1300

    Add comment to ts consumer

--- BookApiConsumer.TypeScript/src/call-api.ts
+++ BookApiConsumer.TypeScript/src/call-api.ts
@@ -7,6 +7,9 @@ async function QueryAuthors() {
     let accessToken;
 
     try {
+        // not sure what the best library is for a ts/node client.
+        // for browser based clients https://www.npmjs.com/package/oidc-client would be an option
+        // This is just using a standard http request with the correct parameters
         const response = await superAgent.post('http://localhost:5000/connect/token')
             .send('client_id=book-api-consumer&client_secret=secret&grant_type=client_credentials');
 
